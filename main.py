bf37a4ca0ba77066ef55d605e4ab2418422cf9ef
Rana Bakri <rana.bakri@rawdalsaleheen.edu.kw>
2022-10-04 17:00:48 +0300

    bareed class after updated

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ class Wallet:
 
     def credit(self,amount):
         self.money+=amount
-        print(f"thank for crdit the {amount} your balance amount now is{self.amount}")
+        print(f"thank for crdit the {amount} your balance amount now is{self.money}")
 
     def debit(self,amount):
        self.money-=amount
@@ -23,17 +23,17 @@ wallet.credit(5)
 
 
 class Person:
-    def __init__(self, name,location,maney):
+    def __init__(self, name,location,wallet):
         self.name=name
         self.location=location
-        self.wallet=Wallet(maney)
+        self.wallet=Wallet(wallet)
 
     # def__str__(self):
     #     return f"this person is{self.name} and it has {self.location}and they have {self.wallet} money"
    
     def move_to_point(self,point):
         
-        self.location =self.location + point
+        self.location = point
         print(f"the new location for you now is{self.location}")
 
     
@@ -52,19 +52,19 @@ class Vendor(Person):
     def sellTo(self,customer, number_of_icecreams):
         coast = number_of_icecreams*self.price
         self.move_to_point(customer.location)
-        customer.wallet.debt(coast)
-        print(f"we sold around {number_of_icecreams} and it coast {coast}   the amount in the vendor wallet now {Vendor.wallet}")
+        customer.wallet.debit(coast)
+        self.wallet.credit(coast)
+        print(f"we sold around {number_of_icecreams} and it coast {coast}   the amount in the vendor wallet now {self.wallet}")
 
-vendor = Vendor("Abdallah",2,10)
 
-vendor.sellTo(7,3)
+
 
 class Customer(Person):
     def __init__(self, name,location,money):
         super().__init__(name,location,money)
 
     def is_in_range(self,vendor):
-        range =abs(vendor-self.location)
+        range =abs(vendor.location -self.location)
         print(f"this vendor {vendor.name}  is withen {self.name} range")
         if range<=vendor.range:
             return True
@@ -80,16 +80,18 @@ class Customer(Person):
             return False
 
     def request_icecream(self,vendor, number_of_icecreams):
-       if self._is_in_range(vendor) and self._have_enough_money(vendor, number_of_icecreams):
-        vendor.sell_to(self, number_of_icecreams)
+       if self.is_in_range(vendor) and self.have_enough_money(vendor, number_of_icecreams):
+        vendor.sellTo( customer, number_of_icecreams)
         print("give the custmer ice-cream")
        else:
         print("sorry we can't deliver the service")
 
     
 
+vendor = Vendor("Abdallah",2,10)
 customer = Customer("Abdallah", 3,6)
 # /print(customer.is_in_range(2))
-print(customer.have_enough_money(2,3))
+# print(customer.have_enough_money(vendor,3))
+customer.request_icecream(vendor,5)
   
  
